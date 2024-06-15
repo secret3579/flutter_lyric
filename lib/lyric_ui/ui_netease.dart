8e6183155c4ab46a953abb941738382d5ad7e898
Henry <henry@newbitcoincity.com>
2024-06-15 20:57:59 +0700

    custom color text

--- lib/lyric_ui/ui_netease.dart
+++ lib/lyric_ui/ui_netease.dart
@@ -4,6 +4,10 @@ import 'package:flutter_lyric/lyric_ui/lyric_ui.dart';
 ///Sample Netease style
 ///should be extends LyricUI implementation your own UI.
 ///this property only for change UI,if not demand just only overwrite methods.
+///
+///
+///
+
 class UINetease extends LyricUI {
   double defaultSize;
   double defaultExtSize;
@@ -16,17 +20,29 @@ class UINetease extends LyricUI {
   bool highlight;
   HighlightDirection highlightDirection;
 
-  UINetease(
-      {this.defaultSize = 18,
-      this.defaultExtSize = 14,
-      this.otherMainSize = 16,
-      this.bias = 0.5,
-      this.lineGap = 25,
-      this.inlineGap = 25,
-      this.lyricAlign = LyricAlign.CENTER,
-      this.lyricBaseLine = LyricBaseLine.CENTER,
-      this.highlight = true,
-      this.highlightDirection = HighlightDirection.LTR});
+  // color text
+  Color? playingExtTextColor;
+  Color? playingMainTextColor;
+  Color? otherExtTextColor;
+  Color? otherMainTextColor;
+
+  UINetease({
+    this.defaultSize = 18,
+    this.defaultExtSize = 14,
+    this.otherMainSize = 16,
+    this.bias = 0.5,
+    this.lineGap = 25,
+    this.inlineGap = 25,
+    this.lyricAlign = LyricAlign.CENTER,
+    this.lyricBaseLine = LyricBaseLine.CENTER,
+    this.highlight = true,
+    this.highlightDirection = HighlightDirection.LTR,
+    // color text
+    this.playingExtTextColor,
+    this.playingMainTextColor,
+    this.otherExtTextColor,
+    this.otherMainTextColor,
+  });
 
   UINetease.clone(UINetease uiNetease)
       : this(
@@ -40,25 +56,34 @@ class UINetease extends LyricUI {
           lyricBaseLine: uiNetease.lyricBaseLine,
           highlight: uiNetease.highlight,
           highlightDirection: uiNetease.highlightDirection,
+          // color text
+          playingExtTextColor: uiNetease.playingExtTextColor,
+          playingMainTextColor: uiNetease.playingMainTextColor,
+          otherExtTextColor: uiNetease.otherExtTextColor,
+          otherMainTextColor: uiNetease.otherMainTextColor,
         );
 
   @override
-  TextStyle getPlayingExtTextStyle() =>
-      TextStyle(color: Colors.grey[300], fontSize: defaultExtSize);
-
-  @override
-  TextStyle getOtherExtTextStyle() => TextStyle(
-        color: Colors.grey[300],
+  TextStyle getPlayingExtTextStyle() => TextStyle(
+        color: playingExtTextColor ?? Colors.grey[300],
         fontSize: defaultExtSize,
       );
 
   @override
-  TextStyle getOtherMainTextStyle() =>
-      TextStyle(color: Colors.grey[200], fontSize: otherMainSize);
+  TextStyle getOtherExtTextStyle() => TextStyle(
+        color: otherExtTextColor ?? Colors.grey[300],
+        fontSize: defaultExtSize,
+      );
+
+  @override
+  TextStyle getOtherMainTextStyle() => TextStyle(
+        color: otherMainTextColor ?? Colors.grey[200],
+        fontSize: otherMainSize,
+      );
 
   @override
   TextStyle getPlayingMainTextStyle() => TextStyle(
-        color: Colors.white,
+        color: playingMainTextColor ?? Colors.white,
         fontSize: defaultSize,
       );
 
